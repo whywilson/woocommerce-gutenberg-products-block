17cfce5bfd7f967268e8e208919a639752c91e52
Mike Jolley <mike.jolley@me.com>
2022-01-26 14:13:55 +0000

    Report generation

--- .github/compare-assets/index.js
+++ .github/compare-assets/index.js
@@ -50,6 +50,9 @@ const runner = async () => {
 		const oldArtifactName = core.getInput( 'old-artifact-name', {
 			required: true,
 		} );
+		const prNumber = github.context.payload.pull_request.number;
+		const owner = context.payload.sender.login;
+		const repo = context.payload.repository.name;
 		const octokit = github.getOctokit( token );
 
 		const newAssets = await downloadAssetsFromArtifact(
@@ -78,10 +81,18 @@ const runner = async () => {
 			Object.entries( newAssets )
 				.map( ( [ key, { dependencies = [] } ] ) => {
 					const oldDependencies = oldAssets[ key ].dependencies || [];
-					const currentChanges = dependencies.filter(
+					/*const added = dependencies.filter(
 						( dependency ) =>
 							! oldDependencies.includes( dependency )
 					);
+					const removed = oldDependencies.filter(
+						( dependency ) =>
+							! dependencies.includes( dependency )
+					);*/
+					const currentChanges = {
+						added = [],
+						removed = [],
+					};
 					return currentChanges.length
 						? [ key, currentChanges ]
 						: null;
@@ -90,6 +101,46 @@ const runner = async () => {
 		);
 
 		console.log( changes );
+
+		const reportContent = Object.entries(changes).map( ( [ handle, { added, removed } ] ) => {
+			const addedDeps = added.length ? '`' + added.implode( '`, `' ) + '`' : '';
+			const removedDeps = removed.length ? '`' + removed.implode( '`, `' ) + '`' : '';
+
+			let icon = '';
+
+			if ( added.length && removed.length ) {
+				icon = '❓';
+			} else if (added.length ) {
+				icon = '⚠️';
+			} else if (removed.length ) {
+				icon = '🎉';
+			}
+
+			return `| \`${ handle }\` | ${ addedDeps } | ${ removedDeps } | ${ icon } |`;
+		} );
+
+		let message = `
+		# Script Dependencies Report
+
+		The \`compare-assets\` action has detected some changed script dependencies between this branch and trunk.
+		Please review and confirm the following are correct before merging. Failure to do so may result in reduced
+		performance since more scripts will be enqueued by WordPress.
+
+		---
+
+		| Script Handle | Added | Removed | |
+		| ------------- | ------| ------- | |
+		${ reportContent }
+
+		__This comment was automatically added by the \`./github/compare-assets\` action.__
+		`;
+
+		octokit.issues.createComment( {
+			owner,
+			repo,
+			issue_number: prNumber,
+			body: message,
+		} );
 	} catch ( error ) {
 		core.setFailed( error.message );
 	}
