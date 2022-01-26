b666642ba8ff4d64feaa60c5be45a0a78ec3b0fd
Mike Jolley <mike.jolley@me.com>
2022-01-26 15:07:09 +0000

    Differnet method of obtaining assets

--- .github/compare-assets/index.js
+++ .github/compare-assets/index.js
@@ -3,62 +3,20 @@
  */
 const github = require( '@actions/github' );
 const core = require( '@actions/core' );
-const unzipper = require( 'unzipper' );
-
-async function downloadAssetsFromArtifact( octokit, artifactName ) {
-	const {
-		data: { artifacts },
-	} = await octokit.rest.actions.listWorkflowRunArtifacts( {
-		...github.context.repo,
-		run_id: github.context.payload.workflow_run.id,
-	} );
-
-	const matchArtifact = artifacts.find(
-		( artifact ) => artifact.name === artifactName
-	);
-
-	if ( ! matchArtifact ) {
-		// None found in this run.
-		return;
-	}
-
-	const download = await octokit.rest.actions.downloadArtifact( {
-		...github.context.repo,
-		artifact_id: matchArtifact.id,
-		archive_format: 'zip',
-	} );
-
-	const { files } = await unzipper.Open.buffer(
-		Buffer.from( download.data )
-	);
-	const fileBuffers = await Promise.all(
-		files.map( ( file ) => file.buffer() )
-	);
-	const parsedFiles = fileBuffers.map( ( buffer ) =>
-		JSON.parse( buffer.toString() )
-	);
-
-	return parsedFiles;
-}
 
 const runner = async () => {
 	try {
 		const token = core.getInput( 'repo-token', { required: true } );
-		const newArtifactName = core.getInput( 'new-artifact-name', {
-			required: true,
-		} );
-		const oldArtifactName = core.getInput( 'old-artifact-name', {
-			required: true,
-		} );
 		const prNumber = github.context.payload.pull_request.number;
 		const owner = github.context.payload.sender.login;
 		const repo = github.context.payload.repository.name;
 		const octokit = github.getOctokit( token );
 
-		const newAssets = await downloadAssetsFromArtifact(
-			octokit,
-			newArtifactName
-		);
+		const newAssets = require( './' +
+			core.getInput( 'new-assets-dir', {
+				required: true,
+			} ) +
+			'/assets.json' );
 
 		console.log( newAssets );
 
@@ -66,10 +24,11 @@ const runner = async () => {
 			return;
 		}
 
-		const oldAssets = await downloadAssetsFromArtifact(
-			octokit,
-			oldArtifactName
-		);
+		const oldAssets = require( './' +
+			core.getInput( 'old-assets-dir', {
+				required: true,
+			} ) +
+			'/assets.json' );
 
 		console.log( oldAssets );
 
